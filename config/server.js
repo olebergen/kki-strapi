f6bc2a887ad0341684130a4e0852e9d84a8c4601
olebergen <ole.bergen.92@gmail.com>
2022-10-12 15:27:21 +0200

    update config directory

--- config/server.js
+++ config/server.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '8ae2732bc6e083cc79dd32bbabe9388b'),
-    },
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
