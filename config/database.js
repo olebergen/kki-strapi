81be18cb4d8bffb28d293b7d10f28337ff18e538
olebergen <ole.bergen.92@gmail.com>
2022-10-13 13:44:07 +0200

    update config and packages

--- config/database.js
+++ config/database.js
@@ -1,11 +1,16 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
+  // defaultConnection: "default",
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "0000"),
+      schema: env("DATABASE_SCHEMA", "public"),
     },
-    useNullAsDefault: true,
   },
 });
